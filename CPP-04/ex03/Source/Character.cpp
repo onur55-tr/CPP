b3f2980bd5acc4afe23abba6bc7b7434f9deda79
Onur Dursun <58361733+onur55-tr@users.noreply.github.com>
2022-05-18 17:06:29 +0300

    coming soon

--- CPP-04/ex03/Source/Character.cpp
+++ CPP-04/ex03/Source/Character.cpp
@@ -1,3 +1,30 @@
 #include "Character.hpp"
 #include "Cure.hpp"
 #include "Ice.hpp"
+
+Character::Character ( void )
+{
+	unsigned  int i = - 1;
+
+	while (++i < 4)
+		materia[i] = 0;
+}
+
+Character::Character ( std::string const &name ) : this->name ( name )
+{
+	unsigned  int i = - 1;
+
+	std::cout << name << " Character created" << std::endl;
+
+	while (++i < 4)
+		materia[i] = 0;
+}
+
+Character::Character ( const Character &src ) { *this = src; }
+
+Character::~Character ( void )
+{
+	for ( unsigned int i = 0; i < 4; i++ )
+		if ( materia[i] != 0)
+			delete ( materia[i] );
+}
